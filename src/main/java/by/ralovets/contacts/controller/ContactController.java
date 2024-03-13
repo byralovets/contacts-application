9c9be091a88a9d93614f7a3e21643b73c7a4538c
Anton Ralovets <by.ralovets@gmai.com>
2024-03-13 05:13:09 +0300

    Create controller for contacts

---
+++ src/main/java/by/ralovets/contacts/controller/ContactController.java
@@ -0,0 +1,45 @@
+package by.ralovets.contacts.controller;
+
+import by.ralovets.contacts.data.request.CreateContactDTO;
+import by.ralovets.contacts.data.request.UpdateContactDTO;
+import by.ralovets.contacts.data.response.ContactDTO;
+import by.ralovets.contacts.facade.ContactFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/contacts")
+@RequiredArgsConstructor
+public class ContactController {
+
+    private final ContactFacade contactFacade;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ContactDTO create(@RequestBody CreateContactDTO data) {
+        return contactFacade.createContact(data);
+    }
+
+    @GetMapping
+    public Page<ContactDTO> read(@RequestParam Integer page, @RequestParam Integer size) {
+        return contactFacade.findContacts(page, size);
+    }
+
+    @GetMapping("/{id}")
+    public ContactDTO read(@PathVariable Long id) {
+        return contactFacade.findContactById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ContactDTO update(@PathVariable Long id, @RequestBody UpdateContactDTO data) {
+        return contactFacade.updateContactById(id, data);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        contactFacade.deleteContactById(id);
+    }
+}
